4ac20c035ccbee1cd6f16aeb5660d5b56a76d480
Justin Taylor <jt@justintaylor.org>
2023-09-20 16:09:45 +0100

    Update widget-mixin.js
    Added hard-coded responsive breakpoints

--- view/base/web/js/content-type/slider/appearance/default/widget-mixin.js
+++ view/base/web/js/content-type/slider/appearance/default/widget-mixin.js
@@ -16,22 +16,48 @@ define([
     return function (sliderWidget) {
         return wrapper.wrap(sliderWidget, function (originalSliderWidgetFunction, config, sliderElement) {
             originalSliderWidgetFunction(config, sliderElement);
-
+    
             const $element = $(sliderElement);
-
+    
             $element.slick(
                 'slickSetOption',
                 'slidesToShow',
                 $element.data('slides-to-show') ?? 1,
                 false
             );
-
+    
             $element.slick(
                 'slickSetOption',
                 'slidesToScroll',
                 $element.data('slides-to-scroll') ?? 1,
                 true
             );
+    
+            // Responsive settings
+            $element.slick('slickSetOption', 'responsive', [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ], true);
         });
     };
+
 });
